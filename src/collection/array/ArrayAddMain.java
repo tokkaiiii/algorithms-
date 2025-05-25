fa3d829dc8691ab2b3a739976025744a86242586
heosangbeom <luv_3_u@naver.com>
2025-05-25 16:00:44 +0900

    feat: 배열의 첫번째 index에 값 추가하는 메소드 생성

--- src/collection/array/ArrayAddMain.java
+++ src/collection/array/ArrayAddMain.java
@@ -1,4 +1,26 @@
 package collection.array;
 
+import java.util.Arrays;
+
+/**
+ * 첫 번째 index에 값을 추가하는 예제입니다.
+ */
 public class ArrayAddMain {
+
+  public static void main(String[] args) {
+    final int[] arr = {1, 2, 0, 0, 0};
+
+    int newValue = 10; // 추가할 값
+    addFirst(arr, newValue);
+
+    System.out.println("Arrays.toString(arr) = " + Arrays.toString(arr));
+  }
+
+  private static void addFirst(int[] arr, int newValue) {
+    for (int index = arr.length - 1; index > 0; index--) {
+      arr[index] = arr[index - 1];
+    }
+
+    arr[0] = newValue; // 첫 번째 index에 값을 추가합니다.
+  }
 }
